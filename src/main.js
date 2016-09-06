ff80fae4b6fa8480ffa5b81380c30c1049bb0ef1
Lukasz Kubica <kubicalukasz92@gmail.com>
2016-09-06 20:26:41 +0200

    change home container

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react'
-// import { StatusBar } from 'react-native'
 import { Provider } from 'react-redux'
 import createStore from './store/createStore'
 import Router from './scenes/router'
 
 class App extends Component {
-
-	// componentWillMount() {
-	// 	StatusBar.setHidden(false);
-	// }
-
 	render() {
 	    return (
 	      <Provider store={createStore()}>
