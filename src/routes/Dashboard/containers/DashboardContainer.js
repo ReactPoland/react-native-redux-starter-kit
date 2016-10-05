74ff915892e31f3dfbb42975098123225f523dec
przeor <kamil@przeorski.pl>
2016-10-05 13:03:19 +0200

    migration 1 - copied a dashboard with a placeholder

---
+++ src/routes/Dashboard/containers/DashboardContainer.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { 
+  dashboardVisitIncrement, 
+  dashboardAddItem,
+  dashboardEditItem 
+} from '../modules/dashboard'
+import Dashboard from 'components/Dashboard'
+
+const mapActionCreators = {
+  dashboardVisitIncrement: () => dashboardVisitIncrement(1),
+  dashboardAddItem: (value) => dashboardAddItem(value),
+  dashboardEditItem: (value) => dashboardEditItem(value)
+}
+
+const mapStateToProps = (state) => ({
+  dashboard: state.dashboard
+})
+
+
+class DashboardContainer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.inputOnChange = this.inputOnChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+    this.itemOnEdit = this.itemOnEdit.bind(this)
+
+
+    this.state = {
+      inputValue: '',
+      editedItemIndex: null
+    }
+  }
+
+  componentDidMount() {
+    this.props.dashboardVisitIncrement();
+  }
+
+  inputOnChange(e) {
+    this.setState({ inputValue: e.target.value })
+  }
+
+  itemOnEdit(itemIndex) {
+    const editedItem = this.props.dashboard.dashboardItems[itemIndex]
+    this.setState({ inputValue: editedItem.label, editedItemIndex: itemIndex })
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    const val = this.state.inputValue
+    const editedItemIndex = this.state.editedItemIndex
+    if(val && editedItemIndex !== null) {
+      this.props.dashboardEditItem({ val, editedItemIndex })
+      this.setState({ inputValue: '', editedItemIndex: null })
+    } else if(val) {
+      this.props.dashboardAddItem(val)
+      this.setState({ inputValue: '' })
+    } else {
+      alert(`Value can't be empty`)
+    }
+  }
+
+  render () {
+    return (
+      <View>
+        <Text>
+          Dashboard React Native Placeholder
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapActionCreators)(DashboardContainer)
