8f90aac6747d5348e58fa0163ec7765fc0d12340
przeor <kamil@przeorski.pl>
2016-10-07 09:16:45 +0200

    dashboard route improv1

--- src/routes/Dashboard/index.js
+++ src/routes/Dashboard/index.js
@@ -1,24 +1,9 @@
-import { injectReducer } from '../../store/reducers'
+import DashboardContainer from './containers/DashboardContainer'
+import reducer from './modules/dashboardReducer'
 
-export default (store) => ({
+export default {
   path: 'dashboard',
-  /*  Async getComponent is only invoked when route matches   */
-  getComponent (nextState, cb) {
-    /*  Webpack - use 'require.ensure' to create a split point
-        and embed an async module loader (jsonp) when bundling   */
-    require.ensure([], (require) => {
-      /*  Webpack - use require callback to define
-          dependencies for bundling   */
-      const Dashboard = require('./containers/DashboardContainer').default
-      const reducer = require('./modules/dashboard').default
-
-      /*  Add the reducer to the store on key 'dashboard'  */
-      injectReducer(store, { key: 'dashboard', reducer })
-
-      /*  Return getComponent   */
-      cb(null, Dashboard)
-
-    /* Webpack named bundle   */
-    }, 'dashboard')
-  }
-})
+  title: 'Dashboard',
+  component: DashboardContainer,
+  reducer: reducer
+}
